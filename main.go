d2c6c2cb7e6b5a79426dd37698f61526e0a17bb9
Ben Serrano <b.serrano8970@gmail.com>
2022-10-01 11:56:23 -0500

    add ability to start frontend from go

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 
 func main() {
 	r := gin.Default()
+
+	// r.Static("/out", "./out")
+	// r.Use(static.Serve("/", static.LocalFile("./out", true)))
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
