1b00673db27f722082986428845cfec8ced0b669
Ben Serrano <b.serrano8970@gmail.com>
2022-10-02 11:47:59 -0500

    Update to use new ui

--- out/ww-script.js
+++ out/ww-script.js
@@ -25,6 +25,7 @@ function addPlaceMarker() {
 
 // Create a WorldWindow for the canvas.
 var wwd = new WorldWind.WorldWindow("canvasOne");
+var highlightController = new WorldWind.HighlightController(wwd);
 
 // Add some image layers to the WorldWindow's globe.
 wwd.addLayer(new WorldWind.BMNGOneImageLayer());
@@ -113,8 +114,8 @@ placemarkLayer.addRenderable(placemark);
 var lat = 10.0;
 var lon = -125.0;
 var alt = 800000.0;
-
-fetch("https://iss-go-backend-z6hx3vadea-uc.a.run.app/getIssLocation")
+// // --- started working on line for space station ---
+fetch("https://iss-go-backend-dyzzyvubfq-uc.a.run.app/getIssLocation")
   .then((res) => res.json())
   .then((data) => {
     const { latitude, longitude, altitude } = data;
@@ -123,33 +124,49 @@ fetch("https://iss-go-backend-z6hx3vadea-uc.a.run.app/getIssLocation")
   })
   .catch((err) => console.log("ewwow"));
 
-var position = new WorldWind.Position(lat, lon, alt);
-var config = { dirPath: "/models/" };
-
-var placemark = addPlaceMarker();
-
-var pathAttributes = new WorldWind.ShapeAttributes(null);
-var positionArray = [];
-positionArray.push(position);
+var pathPositions = [];
 
 // Create the path.
-var path = new WorldWind.Path(positionArray, null);
-path.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
+var path = new WorldWind.Path(pathPositions, null);
+path.altitudeMode = WorldWind.RELATIVE_TO_GROUND; // The path's altitude stays relative to the terrain's altitude.
 path.followTerrain = true;
-path.useSurfaceShapeFor2D = true;
+path.pathType = WorldWind.LINEAR;
+path.extrude = true; // Make it a curtain.
+path.useSurfaceShapeFor2D = true; // Use a surface shape in 2D mode.
 
 // Create and assign the path's attributes.
 var pathAttributes = new WorldWind.ShapeAttributes(null);
 pathAttributes.outlineColor = WorldWind.Color.YELLOW;
-pathAttributes.interiorColor = new WorldWind.Color(0, 1, 1, 1);
-pathAttributes.drawVerticals = path.extrude;
+pathAttributes.interiorColor = new WorldWind.Color(0, 1, 1, 0.5);
+// pathAttributes.drawVerticals = path.extrude; //Draw verticals only when extruding.
 path.attributes = pathAttributes;
 
-// Add the path to a layer and the layer to the WorldWindow's layer list.
+// Create and assign the path's highlight attributes.
+var highlightAttributes = new WorldWind.ShapeAttributes(pathAttributes);
+highlightAttributes.outlineColor = WorldWind.Color.RED;
+highlightAttributes.interiorColor = new WorldWind.Color(1, 1, 1, 0.5);
+path.highlightAttributes = highlightAttributes;
+
+// Now set up to handle highlighting.
+var highlightController = new WorldWind.HighlightController(wwd);
 var pathsLayer = new WorldWind.RenderableLayer();
-pathsLayer.displayName = "Paths";
-pathsLayer.addRenderable(path);
-wwd.addLayer(pathsLayer);
+
+fetch("https://iss-go-backend-dyzzyvubfq-uc.a.run.app/getPastFuturePresentIssLocation")
+  .then((res) => res.json())
+  .then((data) => {
+    pathPositions = data;
+    // console.log(JSON.stringify(pathPositions))
+    path = new WorldWind.Path(pathPositions, pathAttributes);
+    pathsLayer.displayName = "Paths";
+    pathsLayer.addRenderable(path);
+    wwd.addLayer(pathsLayer);
+  })
+  .catch((err) => console.log("ewwow"));
+
+var position = new WorldWind.Position(lat, lon, alt);
+var config = { dirPath: "/models/" };
+
+var placemark = addPlaceMarker();
 
 var colladaLoader = new WorldWind.ColladaLoader(position, config);
 var oneCycle = true;
@@ -157,8 +174,9 @@ var oneCycle = true;
 colladaLoader.load("ISSComplete1.dae", function (colladaModel) {
   colladaModel.scale = 500000;
   modelLayer.addRenderable(colladaModel);
+  modelLayer.addRenderable(placemark);
   window.setInterval(function () {
-    fetch("https://iss-go-backend-z6hx3vadea-uc.a.run.app/getIssLocation")
+    fetch("https://iss-go-backend-dyzzyvubfq-uc.a.run.app/getIssLocation")
       .then((res) => res.json())
       .then((data) => {
         const { latitude, longitude, altitude } = data;
@@ -168,13 +186,11 @@ colladaLoader.load("ISSComplete1.dae", function (colladaModel) {
       })
       .catch((err) => console.log("ewwow"));
 
-    console.log(lat, lon, alt);
-
     position = new WorldWind.Position(lat, lon, alt);
 
     // Placemark label recording
     placemark.label =
-      "Placemark\n" +
+      "ISS Space Station \n" +
       "Lat " +
       modelLayer.renderables[0].position.latitude.toPrecision(4).toString() +
       "\n" +
@@ -182,6 +198,7 @@ colladaLoader.load("ISSComplete1.dae", function (colladaModel) {
       modelLayer.renderables[0].position.longitude.toPrecision(5).toString();
     // ISS Model position updating
     modelLayer.renderables[0].position = position;
+    modelLayer.renderables[1].position = position;
     wwd.redraw();
   }, 1000);
 });
